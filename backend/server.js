f9979afd8eb93df2b57430581768cb216471afb2
Bharat Singh <bharatsre11@gmail.com>
2026-04-18 15:59:12 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const orderRoutes = require("./routes/Order");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
